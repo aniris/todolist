05d21cd691a558484fd9a7c634d33f31b546a017
Irina Soloveva <aniri.solovyeva@gmail.com>
2018-04-16 01:20:31 +0300

    edit server.j for routing

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 
 var express = require('express');
+var history = require('connect-history-api-fallback');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
+app.use(history({
+  // verbose: true
+}));
 app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
+
+// var express = require('express');
+// var app = express();
+// var path = require('path');
+//
+
+//
+// app.use(express.static(__dirname + '/dist'));
+//
+// app.set('port', (process.env.PORT || 8080));
+//
+// app.listen(app.get('port'), () => {
+//   console.log(`ENV: ${process.env.NODE_ENV}`);
+//   console.log(`Derp is running at localhost: ${app.get('port')}`);
+// });
+
