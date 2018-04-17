af5409854413c3b9d6aa24a24d2ce3ed187daef5
Irina Soloveva <aniri.solovyeva@gmail.com>
2018-04-17 15:02:21 +0300

    Размещает информацию на главной странице

--- server.js
+++ server.js
@@ -11,19 +11,3 @@ app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
-
-// var express = require('express');
-// var app = express();
-// var path = require('path');
-//
-
-//
-// app.use(express.static(__dirname + '/dist'));
-//
-// app.set('port', (process.env.PORT || 8080));
-//
-// app.listen(app.get('port'), () => {
-//   console.log(`ENV: ${process.env.NODE_ENV}`);
-//   console.log(`Derp is running at localhost: ${app.get('port')}`);
-// });
-
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
-    {{msg}}
+  <div class="main">
+    <h1 class="main__header">Это приложение создано для пробы VueJS</h1>
+    <div class="description">
+      На данный момент реализован <router-link to='todo' class="description__link">ToDo</router-link> с возможностями:
+      <ul class="description__list">
+        <li class="description__link">Добавить задачу</li>
+        <li class="description__link">Удалить задачу</li>
+        <li class="description__link">Отредактировать задачу</li>
+      </ul>
+      Задача состоит из:
+      <ul class="description__list">
+        <li class="description__link">Текста</li>
+        <li class="description__link">Приоритета</li>
+      </ul>
+      Хранение списка задач организованно в localStorage
+    </div>
   </div>
 </template>
 
@@ -9,12 +23,37 @@ export default {
   name: 'Main',
   data () {
     return {
-      msg: 'main'
     }
   }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.main {
+  width: fit-content;
+  margin: 20px auto 0;
+  &__header {
+    font-family: 'Open Sans';
+    color: #006175;
+    text-shadow: 0 1px 0 #ccc;
+    padding-bottom: 10px;
+    margin-bottom: 25px;
+    border-bottom: 1px solid #006175;
+    font-size: 26px
+  }
+}
+.description {
+  font-family: 'Roboto';
+  font-size: 16px;
+  color: #222;
+  &__link {
+    color: #006175;
+  }
+  &__list {
+    background-color: #ffffff;
+    margin: 15px 0;
+    border-left: 5px solid #00a5bf;
+    padding: 15px;
+  }
+}
 </style>
